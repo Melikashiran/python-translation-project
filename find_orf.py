1d89447f38238c309e751d2313a5f06cf79b23b3
Melika <Mzg0146@auburn.edu>
2026-04-19 12:11:22 -0500

    adding the third test

--- find_orf.py
+++ find_orf.py
@@ -208,7 +208,8 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    #orf_pattern_str = r'AUGGUAUAA'
+    orf_pattern_str = r'AUG(?:[AUCG]{3})*?(?:UAA|UAG|UGA)'
     ##########################################################################
 
     # Create the regular expression object
